54a27f3012ae05843f1daf45be54b23372f68d45
hannelita <hannelita@gmail.com>
2018-01-24 13:21:21 -0500

    New lazy loading implemented; fixing parser according to the new CSS

--- amazon_spider.py
+++ amazon_spider.py
@@ -1,7 +1,8 @@
 import scrapy
+import logging
 
 class AmazonSpider(scrapy.Spider):
-    name = 'amazonpider'
+    name = 'amazonspider'
     start_urls = ['https://www.amazon.com.br/gp/registry/wishlist/3DA4I0ZLH8ADW']
 
     def parse(self, response):
@@ -10,15 +11,17 @@ class AmazonSpider(scrapy.Spider):
       for book in books:
         book_info = book.xpath('.//div[re:test(@id, "itemInfo_*")]')
         title = self.strformat(book_info.xpath('.//a[re:test(@id, "itemName_*")]/text()').extract_first())
-        authors_format = reduce(lambda a, b: a+b, map(lambda x: x.strip() ,book_info.xpath('.//div[@class="a-row a-size-small"]/text()').extract()))
-        price = self.strformat(book_info.xpath('.//span[re:test(@id, "itemPrice_*")]/text()').extract_first())
+        authors_format = ''
+        if book_info.xpath('.//div[@class="a-row a-size-small"]/span/text()'):
+          authors_format = reduce(lambda a, b: a+b, map(lambda x: x.strip(), book_info.xpath('.//div[@class="a-row a-size-small"]/span/text()').extract()))
+        price = self.strformat(book_info.xpath('.//span[re:test(@id, "itemPrice_*")]//span/text()').extract_first())
 
         yield {'Title':title, 'Info':authors_format, 'Last price':price }
 
-      pagination_ref = response.css("#wishlistPagination")
-      next_page = pagination_ref.xpath('.//li[@class="a-last"]//a//@href').extract_first()
-      if next_page:
-        yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+      next_page_ref = response.css('#sort-by-price-load-more-items-url').xpath('.//@value').extract_first()
+      next_page = "https://www.amazon.com.br" + next_page_ref
+      if "lek=&" not in next_page:
+        yield scrapy.Request(next_page, callback=self.parse)
 
 
     def strformat(self, data):
